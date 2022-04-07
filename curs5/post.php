a07bf5c378f5549013c2ab1c472499076ab92454
unknown <stefan.ionica2000@gmail.com>
2022-04-07 20:50:25 +0300

    websitejs

---
+++ curs5/post.php
@@ -0,0 +1,12 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+if(isset($_POST) && !empty($_POST)){
+    //print_r($_POST); die;
+
+    echo "Datele au fost salvate cu scucces 2022!";
+}
+
+?>
